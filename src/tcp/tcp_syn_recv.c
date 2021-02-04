fda04a2bd31346ae08a07e6ea6ada1305f3a9664
Ziyan Wu <ziyanwu93@gmail.com>
2021-02-03 21:22:35 -0600

    update

--- src/tcp/tcp_syn_recv.c
+++ src/tcp/tcp_syn_recv.c
@@ -6,7 +6,7 @@
 
 void handle_tcp_syn_recv(struct tcp *_tcp, struct connection *_connection, struct quad q, struct rte_tcp_hdr *tcpHdr,
                          struct rte_ipv4_hdr *ipv4Hdr, void *data, int size) {
-//    printf("handle_tcp_syn_recv called\n");
+    printf("handle_tcp_syn_recv called\n");
     uint32_t seq = rte_be_to_cpu_32(tcpHdr->sent_seq);
     uint32_t ackn = rte_be_to_cpu_32(tcpHdr->recv_ack);
 
